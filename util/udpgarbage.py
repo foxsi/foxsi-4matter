7574b0dd2604be41da5a28045237e531c019564a
Thanasi Pantazides <thanasipantazides@gmail.com>
2023-05-12 16:05:00 -0500

    fixing buffer size problem in udpecho.py, fixing remote endpoint in udpgarbage.py

--- util/udpgarbage.py
+++ util/udpgarbage.py
@@ -2,17 +2,21 @@ import socket, time, random
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-address = "192.168.1.8"
-port = 9999
+local_address = "192.168.1.8"
+local_port = 9999
 
-endpoint = (address, port)
-sock.bind(endpoint)
+remote_address = "192.168.1.108"
+remote_port = 10000
+
+local_endpoint = (local_address, local_port)
+remote_endpoint = (remote_address, remote_port)
+sock.bind(local_endpoint)
 
 while True:
     # make some random data to transmit
     data = random.randbytes(1000)
-    print("sending data to " + address + ":" + str(port))
-    sock.sendto(data, endpoint)
+    print("sending data to " + local_address + ":" + str(local_port))
+    sock.sendto(data, remote_endpoint)
 
     # approximate 18 Mbps downlink rate
     time.sleep(55.0/1000)
