2473a89ec431f52cd32fb843a5fcf7b86ba3d349
Thanasi Pantazides <thanasipantazides@gmail.com>
2023-05-22 12:33:34 -0500

    adding hex printing, addresses issue #17

--- include/Utilities.h
+++ include/Utilities.h
@@ -14,8 +14,14 @@ STATE_ORDER operator++(STATE_ORDER& order, int);
 int inc_mod(int i, int n);
 
 void debug_print(std::string msg);
+void hex_print(std::vector<uint8_t>& data);
+void hex_print(std::vector<char>& data);
 
 // find a better place to put this
 uint8_t spw_calculate_crc_F(std::vector<char>& data);
+uint8_t spw_calculate_crc_uint_F(std::vector<uint8_t>& data);
+
+std::vector<char> string_to_chars(std::string hex_str);
+
 
 #endif
