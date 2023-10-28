73cb85ae88e4a2e7e25843953c1ed58337ebdcb6
thanasipantazides <panta013@umn.edu>
2023-10-27 21:07:17 -0700

    checking color printer

--- test/test_buffers.cpp
+++ test/test_buffers.cpp
@@ -19,6 +19,7 @@ int main(int argc, char* argv[]) {
 
     // setup test of buffer pipe
     System cdte1 = deck.get_sys_for_name("cdte1");
+    System cmos1 = deck.get_sys_for_name("cmos1");
     System gse = deck.get_sys_for_name("gse");
 
     std::queue<DownlinkBufferElement> downlink_buffer;
@@ -44,11 +45,16 @@ int main(int argc, char* argv[]) {
 
     // make packets out of false data, add spw header to each:
     size_t false_spw_header_size = 24;
-    size_t packet_size = cdte1.spacewire->max_payload_size - false_spw_header_size;
-    
+    size_t false_spw_footer_size = 1;
+    size_t packet_size = cdte1.spacewire->max_payload_size - false_spw_header_size - false_spw_footer_size;    
+
     std::vector<uint8_t> false_spw_header(false_spw_header_size);
     for (size_t i = 0; i < false_spw_header_size; ++i) {
         false_spw_header[i] = i;
+    }
+    std::vector<uint8_t> false_spw_footer(false_spw_footer_size);
+    for (size_t i = 0; i < false_spw_footer_size; ++i) {
+        false_spw_footer[i] = 0xff;
     }
 
     // assemble vector of packets of false data:
@@ -59,6 +65,8 @@ int main(int argc, char* argv[]) {
         std::vector<uint8_t> this_data(false_data.begin() + i, false_data.begin() + last_element);
         this_packet.insert(this_packet.begin(), false_spw_header.begin(), false_spw_header.end());
         this_packet.insert(this_packet.end(), this_data.begin(), this_data.end());
+        this_packet.insert(this_packet.end(), false_spw_footer.begin(), false_spw_footer.end());
+        
         false_input_packets.push_back(this_packet);
 
         // std::cout << "\nfalse packet (size " << this_packet.size() << "): ";
@@ -77,6 +85,9 @@ int main(int argc, char* argv[]) {
 
     std::cout << "\n";
     std::cout << "PacketFramer::check_frame_done(): " << std::to_string(pf.check_frame_done()) << "\n";
+    if (!pf.check_frame_done()) {
+        std::cout << "PacketFramer::frame.size(): " << std::to_string(pf.get_frame().size()) << "\n";
+    }
 
     // hand the frame over to FramePacketizer:
     fp.set_frame(pf.get_frame());
@@ -95,13 +106,15 @@ int main(int argc, char* argv[]) {
 
     std::cout << "pushed frame to downlink queue. FramePacketizer::frame_emptied(): " << fp.frame_emptied() << "\n";
 
+    std::cout << fp.to_string();
+
     // pop buffer to emulate downlink, and remove header "on ground". Reassemble frame.
     std::vector<uint8_t> false_downlink;
-    DownlinkBufferElement this_concurrent_element;
+    DownlinkBufferElement this_concurrent_element(&cdte1, &gse, RING_BUFFER_TYPE_OPTIONS::PC);
     while(downlink_buffer.size() > 0) {
 
         // try using the std::queue
-        DownlinkBufferElement this_downlink = downlink_buffer.front();
+        DownlinkBufferElement this_downlink(downlink_buffer.front());
         downlink_buffer.pop();
 
         // try using moodycamel::ConcurrentQueue
@@ -129,6 +142,19 @@ int main(int argc, char* argv[]) {
         std::cout << "\n";
     }
 
+    std::cout << "test SpaceWire printing:\n";
+    std::vector<uint8_t> test_cdte_write_msg = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x23, 0x01, 0x02, 0xfe, 0x01, 0x7d, 0x02, 0x00, 0x00, 0x01, 0x03, 0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0c, 0xb5, 0x3c, 0x3c, 0x01, 0x00, 0x01, 0x01, 0x01, 0x01, 0x3c, 0x3c, 0x3c, 0x3c, 0x3e};
+    std::vector<uint8_t> test_cdte_read_msg = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x01, 0x02, 0xfe, 0x01, 0x4d, 0x02, 0x00, 0x00, 0x01, 0x03, 0xfe, 0x00, 0x00, 0x00, 0x00, 0x53, 0xeb, 0xd8, 0x00, 0x07, 0xd0, 0x94};
+    std::vector<uint8_t> test_cdte_reply_msg = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0xfe, 0x01, 0x0d, 0x00, 0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x0e, 0x00, 0x41, 0x00, 0x18, 0xa9};
+
+    auto test_cmos_write_msg = deck.make_spw_packet_for_sys_for_command(cmos1, deck.get_command_for_sys_for_code(cmos1.hex, 0x10));
+    auto test_cmos_read_msg = deck.make_spw_packet_for_sys_for_command(cmos1, deck.get_command_for_sys_for_code(cmos1.hex, 0x92));
+
+    utilities::spw_print(test_cmos_write_msg, cmos1.spacewire);
+    utilities::spw_print(test_cmos_read_msg, cmos1.spacewire);
+    utilities::spw_print(test_cdte_write_msg, cdte1.spacewire);
+    utilities::spw_print(test_cdte_read_msg, cdte1.spacewire);
+    utilities::spw_print(test_cdte_reply_msg, nullptr);
 
     std::cout << "end of main.\n";
     return 0;
