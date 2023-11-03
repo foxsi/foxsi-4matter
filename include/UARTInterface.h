9bceca94e8de1534fe389bee0eb946908bdd010b
Kristopher Cooper <kris@infra04-wg012.eduroam.wireless.umn.edu>
2023-11-03 13:22:33 -0500

    adding updated uart files

--- include/UARTInterface.h
+++ include/UARTInterface.h
@@ -16,15 +16,15 @@
 
 class UARTPort {
 	public:
-        // open the port
-		UARTPort();
+        // open the default UART0/1 port
+		UARTPort(const char* port_file="/dev/ttyS0");
 
         //unsigned int baud_rate, unsigned short parity_bits, unsigned short data_bits, unsigned short stop_bits …
         struct termios tty; // create TERMIOS structure
         int fd;	// the thing you get back when you open() a port
         int serial2tty(); // send serial port attrs to the tty object
         int attrs2tty(); // set the changed attrs to the tty object
-        int setup(uint32_t baud_rate=B9600, int vtime = 10, int vmin = 0, std::string parity="clear", int stop_bits=1, int bits_in_byte=CS8);
+        int setup(uint32_t baud_rate=B9600, int vtime=10, int vmin=0, std::string parity="clear", int stop_bits=1, int bits_in_byte=CS8);
 
         // methods to set port settings with select options
         int set_parity(std::string parity);
--- src/UARTInterface.cpp
+++ src/UARTInterface.cpp
@@ -47,9 +47,11 @@ int print_bl(int num_bytes, uint8_t* read_buf) {
 
 
 
-UARTPort::UARTPort() {
+UARTPort::UARTPort(const char* port_file) {
     // open the UART port
-    UARTPort::fd = open("/dev/ttyS0", O_RDWR);
+    // UARTPort::fd = open("/dev/ttyS0", O_RDWR);
+    // UARTPort::fd = open("/dev/ttyAMA1", O_RDWR);
+    UARTPort::fd = open(port_file, O_RDWR);
 };
 
 int UARTPort::serial2tty(){
@@ -273,6 +275,10 @@ UARTPort::~UARTPort() {
 
 
 
+const std::vector<uint8_t> FRAME0 = {0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D};
+const std::vector<uint8_t> FRAME1 = {0x03, 0x02, 0x05, 0x07};
+const std::vector<uint8_t> FRAME2 = {0x08, 0x1e, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x6f};
+
 int test0() {
     // send-receive 
 
@@ -283,8 +289,7 @@ int test0() {
     uart.setup();
 
     // send the frame information
-    const std::vector<uint8_t> frame = {0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D, 0x7B, 0x03, 0x02, 0x05, 0x07, 0x7D};
-    uart.send(frame);
+    uart.send(FRAME0);
 
     // define a vector to be used as a buffer 
     std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
@@ -302,8 +307,340 @@ int test0() {
     return 0;
 };
 
+int test1() {
+    // send-receive wait time of 1,000 ms
 
-int main() {
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+
+    // send the frame information
+    uart.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    //delay
+    usleep(1000); //microseconds
+
+    // send buffer to the reading port method
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test1. 1,000 us delay between write and read.\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test2() {
+    // send-receive wait time of 10,000 ms
+
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+
+    // send the frame information
+    uart.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    //delay
+    usleep(10000); //microseconds
+
+    // send buffer to the reading port method
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test2. 10,000 us delay between write and read.\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test3() {
+    // send-receive wait time of 100,000 ms
+
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+
+    // send the frame information
+    uart.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    //delay
+    usleep(100000); //microseconds
+
+    // send buffer to the reading port method
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test3. 100,000 us delay between write and read.\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test4() {
+    // change vtime and vmin while sending messages
+
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+
+    // send the frame information
+    uart.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    // send buffer to the reading port method
+    uart.recv(buffer);
+
+    // try to change the settings to wait and catch full message
+    uart.vsettings(5, FRAME0.size());
+    uart.send(FRAME0);
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new message but the same number of bytes as the first one
+    print_bl(uart.recv_bytes+5, uart.recv_msg);
+
+    // print new line
+    printf("Test4. Same as test0 then change vtime and vmin for the same message again (see what is left in the buffer).\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test5() {
+    // different sized frames
+
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    // try to change the settings to wait and catch full message
+    uart.vsettings(5, FRAME0.size());
+    uart.send(FRAME0);
+    uart.recv(buffer);
+    int num_bytes = uart.recv_bytes;
+
+    // send a smaller frame after a longer one
+    uart.send(FRAME1);
+    uart.recv(buffer);
+
+    // print new message but the same number of bytes as the first one
+    printf("Read %i bytes (1st message value). Received message:\n", num_bytes);
+    print_bl(num_bytes, uart.recv_msg);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test5. First frame is long, second is short.\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test6() {
+    // different sized frames
+
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    // try to change the settings to wait and catch full message
+    uart.vsettings(5, FRAME0.size());
+    uart.send(FRAME0);
+    uart.recv(buffer);
+    int num_bytes = uart.recv_bytes;
+
+    // send a smaller frame after a longer one
+    uart.vsettings(5, FRAME1.size());
+    uart.send(FRAME1);
+    uart.recv(buffer);
+
+    // print new message but the same number of bytes as the first one
+    printf("Read %i bytes (1st message value). Received message:\n", num_bytes);
+    print_bl(num_bytes, uart.recv_msg);
+
+    // send a larger frame after a shorter one
+    uart.vsettings(5, FRAME2.size());
+    uart.send(FRAME2);
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test6. First frame is long, second is short, third is longer again and scale vmin to match.\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test7() {
+    // send-receive 
+
+    // create UART port object and message to send
+    UARTPort uart;
+    
+    // set up the UART port
+    uart.setup();
+    uart.baudsettings(B57600);
+
+    // send the frame information
+    uart.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    // send buffer to the reading port method
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test7. Same as Test0 but with 6xBaud rate.\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test_collection_initial(){
     // simple use case
     test0();
+    // test with 1,000 us between writing and reading 
+    test1();
+    // test with 10,000 us between writing and reading 
+    test2();
+    // test with 100,000 us between writing and reading 
+    test3();
+    // change the vtime and vmin after set-up
+    test4();
+    // change size of message
+    test5();
+    // change size of message and adjust vmin
+    test6();
+    // 6x Baud rate of Test0
+    test7();
+
+    return 0;
+}
+
+int test8() {
+    // send-receive 
+
+    // create UART port object and message to send
+    UARTPort uart("/dev/ttyAMA1");
+    
+    // set up the UART port
+    uart.setup();
+
+    // send the frame information
+    uart.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer(BUFFER_MAX_SIZE);
+
+    // send buffer to the reading port method
+    uart.recv(buffer);
+
+    // make sure to close the port
+    uart.close_fd();
+
+    // print new line
+    printf("Test8. Change from default UART0/1 port (/dev/ttyS0) to UART3 (/dev/ttyAMA1).\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test9() {
+    // send-receive 
+
+    // create UART port object and message to send
+    UARTPort uart01;
+    UARTPort uart3("/dev/ttyAMA1");
+    
+    // set up the UART port
+    uart01.setup();
+    // uart01.vsettings(5, FRAME0.size());
+    uart3.setup();
+    // uart3.vsettings(5, FRAME0.size());
+
+    // send the frame information
+    uart01.send(FRAME0);
+    uart3.send(FRAME0);
+
+    // define a vector to be used as a buffer 
+    std::vector<uint8_t> buffer01(BUFFER_MAX_SIZE);
+    std::vector<uint8_t> buffer3(BUFFER_MAX_SIZE);
+
+    // send buffer to the reading port method
+    uart01.recv(buffer01);
+    uart3.recv(buffer3);
+
+    // make sure to close the port
+    uart01.close_fd();
+    uart3.close_fd();
+
+    // print new line
+    printf("Test9. Test default UART0/1 port (/dev/ttyS0) and UART3 (/dev/ttyAMA1).\n");
+    printf("\n");
+
+    return 0;
+};
+
+int test_collection_port(){
+    // test another port enabled 
+    test8();
+    // test two ports
+    test9();
+
+    return 0;
+}
+
+
+int main() {
+    // initial, simple tests to check default message sending
+    test_collection_initial();
+    // port tests
+    test_collection_port();
 };
